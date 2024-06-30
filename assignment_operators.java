11ae7303b27014eb8651e20d87a27911ec8f0adf
ganeshmande <119119655+ganeshmande@users.noreply.github.com>
2024-06-30 23:53:53 +0530

    Add files via upload

---
+++ assignment_operators.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class assignment_operators
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        a+= 20;    //a=a+20
+        System.out.println(a);
+
+        int b=11;
+        b-=10;    //b=b-10
+        System.out.println(b);
+
+        int c=50;
+        c*=10;    //c=c*10
+        System.out.println(c);
+
+        int d=100;
+        d/=10;    //d=d/10
+        System.out.println(d);        
+    }
+}

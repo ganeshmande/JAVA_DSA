af960b44ec16bf40085162248098a50bab61f255
ganeshmande <119119655+ganeshmande@users.noreply.github.com>
2024-07-09 15:56:58 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics{
+    public static void main (String[] args){
+        int a=24;
+        int b=34;
+        System.out.println(a+'\n');
+        System.out.println(b);
+    }
+}
